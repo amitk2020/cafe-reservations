28903ea23b9578617508ad5fbd1660f2f412c45c
Amit <amit.k03377@gmail.com>
2025-08-16 22:03:29 +1200

    sample test with data

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const ContactForm = () => {
     return (
-        <div className='reservation-form'>
-            <form name="contact" method='post' netlify>
+        <div className="reservation-form">
+            <form
+                name="contact"
+                method="POST"
+                data-netlify="true"
+                netlify-honeypot="bot-field"
+            >
+                {/* This hidden input is required for Netlify to detect the form */}
+                <input type="hidden" name="form-name" value="contact" />
+
+                {/* Honeypot field */}
+                <p hidden>
+                    <label>
+                        Don’t fill this out if you’re human: <input name="bot-field" />
+                    </label>
+                </p>
+
                 <p>
-                    <label>Name <input type="text" name="name" /></label>
+                    <label>
+                        Name <input type="text" name="name" required />
+                    </label>
                 </p>
                 <p>
-                    <label>Email <input type="email" name="email" /></label>
+                    <label>
+                        Email <input type="email" name="email" required />
+                    </label>
                 </p>
                 <p>
                     <button type="submit">Send</button>
                 </p>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default ContactForm
+export default ContactForm;
